5dd2a9e34be4dc8ca80545034f4fa9a2ecb43746
eagleyes005 <samuel.thomas1109@outlook.com>
2022-09-13 17:39:07 -0300

    3

---
+++ src/testeIR.java
@@ -0,0 +1,16 @@
+public class testeIR {
+    public static void main(String[] args) {
+
+        double salario = 2700.0;
+
+        if (salario <= 2600.0) {
+            System.out.println("A sua aliquota é de 15%");
+            System.out.println("Você pode deduzir até R$ 350");
+        }
+
+        if (salario <= 3750.0 & salario > 2600.0) {
+            System.out.println("A sua aliquota é de 22,5%");
+            System.out.println("Você pode deduzir até R$ 636");
+        }
+    }
+}
