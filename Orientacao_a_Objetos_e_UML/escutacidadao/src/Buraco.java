4495c507d69e49565ddd0f7ce91ca17a01802058
eagleyes005 <samuel.thomas1109@outlook.com>
2023-09-25 13:30:44 -0300

    escutacidadao

---
+++ Orientacao_a_Objetos_e_UML/escutacidadao/src/Buraco.java
@@ -0,0 +1,11 @@
+public class Buraco {
+    //Atributos
+    private int id;
+    private int tamanho;
+    private String localizacaoNaRua;
+    private String distrito;
+    private int prioridade;
+    private Endereco endereco;
+    private OrdemDeServico ordemDeServico;
+    private Dano dano;
+}
